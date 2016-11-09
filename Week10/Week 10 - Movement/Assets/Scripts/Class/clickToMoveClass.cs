286a2206b9e79d8d635e5bef4dd79c7ac25abce6
Diego Garcia <radstronomical@gmail.com>
2016-11-08 19:05:49 -0500

    Week 10 class on movement

---
+++ Week10/Week 10 - Movement/Assets/Scripts/Class/clickToMoveClass.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickToMoveClass : MonoBehaviour {
+
+	private Vector3 destination;
+
+	Vector3 move;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//check if the player is clicking
+		if (Input.GetMouseButton(0)) {
+
+		//see where they're clicking
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit) && hit.collider.tag == "ground")
+			{
+				//move the player to that point
+				destination = hit.point;
+
+ 				//make sure the destination is at the player's current height
+				destination = new Vector3(destination.x, transform.position.y, destination.z);
+
+
+
+				move = destination - transform.position;
+
+				move = move.normalized;
+
+
+
+				//transform.position = Vector3.Lerp(transform.position, destination, .2f);
+
+			}
+		}
+	}
+}
